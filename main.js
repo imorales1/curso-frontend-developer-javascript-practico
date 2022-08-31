651501e926404e218b9f52316a3fa4f160a13441
Ismar E. Morales <imorales@sandiego.com.gt>
2022-08-31 00:28:51 -0600

    Fusion menu Mobile

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu =document.querySelector('.desktop-menu');
+const menuHanIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu ');
 
-menuEmail.addEventListener('click', toogleDesktopMenu);
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHanIcon.addEventListener('click', toggleMobileMenu);
 
-function toogleDesktopMenu() {
+function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
 }
 
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
 
